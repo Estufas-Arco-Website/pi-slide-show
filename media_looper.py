0eec62dbe8f7f5fb3e33d390b2207894543d4c3b
Arco <122691531+estufasarco@users.noreply.github.com>
2023-12-05 16:21:49 -0600

    added background music and fade transition

--- media_looper.py
+++ media_looper.py
@@ -1,16 +1,44 @@
 import os
 import pygame
+import random
 from time import sleep
 import subprocess
 
-# Initialize Pygame
+# Initialize Pygame and its mixer
 pygame.init()
+pygame.mixer.init()
 infoObject = pygame.display.Info()
 screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), pygame.FULLSCREEN)
 
-# Path to your media file
+# Paths
 documents_path = "/home/david/Documents/"
 media_files = [os.path.join(documents_path, f) for f in os.listdir(documents_path) if os.path.isfile(os.path.join(documents_path, f)) and f.lower().endswith(('png', 'jpg', 'jpeg', 'mp4', 'avi', 'mov'))]
+audio_files = [os.path.join(documents_path, f) for f in os.listdir(documents_path) if os.path.isfile(os.path.join(documents_path, f)) and f.lower().endswith('mp3')]
+
+# Shuffle audio files
+random.shuffle(audio_files)
+
+# Function to play background music
+def play_background_music():
+    if not pygame.mixer.music.get_busy():
+        if audio_files:
+            next_song = audio_files.pop()
+            pygame.mixer.music.load(next_song)
+            pygame.mixer.music.play()
+        else:  # Re-shuffle if all songs have been played
+            audio_files.extend([os.path.join(documents_path, f) for f in os.listdir(documents_path) if os.path.isfile(os.path.join(documents_path, f)) and f.lower().endswith('mp3')])
+            random.shuffle(audio_files)
+            play_background_music()
+
+# Function to display transition effect
+def transition_effect():
+    for alpha in range(0, 255, 5):
+        screen.fill((0, 0, 0))
+        overlay = pygame.Surface((infoObject.current_w, infoObject.current_h))
+        overlay.set_alpha(alpha)
+        screen.blit(overlay, (0, 0))
+        pygame.display.update()
+        sleep(0.01)
 
 # Function to display image
 def display_image(file_path):
@@ -37,6 +65,7 @@ def display_image(file_path):
     except Exception as e:
         print(f"An error occurred while displaying image: {e}")
 
+# Function to play video
 def play_video(file_path):
     try:
         # VLC command modified to hide on-screen information and speed up start time
@@ -45,7 +74,8 @@ def play_video(file_path):
     except Exception as e:
         print(f"An error occurred while playing video: {e}")
 
-
+# Start playing background music
+play_background_music()
 
 # Main loop
 while True:
@@ -57,6 +87,9 @@ while True:
     for media_file in media_files:
         print(f"Accessing file: {media_file}")
         try:
+            transition_effect()  # Transition effect
+            play_background_music()  # Check and play next song if needed
+
             if media_file.lower().endswith(('png', 'jpg', 'jpeg')):
                 display_image(media_file)
             elif media_file.lower().endswith(('mp4', 'avi', 'mov')):
